c4f33ede187fa1fbb6d499c9d25874faeb6fe797
m-rosinsky <michael.rosinsky@uconn.edu>
2024-06-02 17:05:42 +0000

    Added index.html template

--- scrapr/index.py
+++ scrapr/index.py
@@ -1,9 +1,9 @@
 from flask import (
-    Blueprint, request,
+    Blueprint, render_template
 )
 
 bp = Blueprint('index', __name__)
 
 @bp.route('/', methods=('GET',))
 def index():
-    return 'hello'
+    return render_template('index.html')
